2f13667315e690f58554865c4aeacfc2fe043abf
Romain CORNU <romain.cornu.ext@adevinta.com>
2021-11-18 17:46:32 +0100

    feat: remove environment log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/sh -l
 
+tag=$GITHUB_SHA
 case $GITHUB_REF in
     refs/tags/*        ) environment=production; tag=$GITHUB_REF_NAME ;;
-    refs/heads/main    ) environment=stage; tag=$GITHUB_SHA ;;
-    refs/pull/*        ) environment=pullrequest-${INPUT_NUMBER}; tag=$GITHUB_SHA ;;
+    refs/heads/main    ) environment=stage ;;
+    refs/pull/*        ) environment=pullrequest-${INPUT_NUMBER} ;;
     *                  ) environment=''
 esac
-
-env
 
 echo "::set-output name=environment::$environment"
 echo "::set-output name=application::$(basename $GITHUB_REPOSITORY)"
